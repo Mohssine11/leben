300689b0609c4e2b16991a4c24f4bade5fc0b4f1
Mohssine <enajarmohssine123@gmail.com>
2025-06-27 17:56:14 +0200

    edit

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 composer install
+npm install
+npm run build
 php artisan migrate --force
 php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 php artisan serve --host=0.0.0.0 --port=8080
